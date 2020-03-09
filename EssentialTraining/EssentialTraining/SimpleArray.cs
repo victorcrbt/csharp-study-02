024f703fee011a30585367c0498cc4b55fe69479
Victor Batalha <madivcb@gmail.com>
2020-03-08 22:31:01 -0300

    Simple array

---
+++ EssentialTraining/EssentialTraining/SimpleArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+  public class SimpleArray
+  {
+    public string[] GroceryList { get; set; }
+
+    public SimpleArray()
+    {
+      /**
+       * To declare an array we use the keyword new, the type of the values
+       * and put the length of the array inside brackets. We can immediatly
+       * attribute values by adding them between curly braces right after
+       * the brackets.
+       * 
+       * The array can be resized later if needed.
+       */
+      GroceryList = new string[4] { "Milk", "Cheese", "Eggs", "Bread" };
+    }
+
+    public override string ToString()
+    {
+      return "There are " + GroceryList.Length + " and they are: " + GroceryList.ToString();
+    }
+  }
+}
---
+++ EssentialTraining/EssentialTrainingTests/SimpleArrayTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+  [TestClass]
+  public class SimpleArrayTest
+  {
+    [TestMethod]
+    public void TestIntantiation()
+    {
+      var testInstance = new SimpleArray();
+
+      Assert.AreEqual(4, testInstance.GroceryList.Length, "it should have a length of 4");
+      Assert.AreEqual("Cheese", testInstance.GroceryList[1]);
+    }
+
+    [TestMethod]
+    public void TestToString()
+    {
+      var testInstance = new SimpleArray();
+
+      Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+    }
+  }
+}
