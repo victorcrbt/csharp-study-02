a9fa3902c0e5d209a0fa0abe82d157c67271cd92
Victor Batalha <madivcb@gmail.com>
2020-03-08 23:30:53 -0300

    Dictionary

---
+++ EssentialTraining/EssentialTraining/DictionaryWords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+  public class DictionaryWords
+  {
+    /**
+     * Dictionaries works like arrays as well but with the
+     * feature of a key/value structure. So, to define the
+     * type of a dictionary (which is also a generic), we
+     * pass the type of the key and the value between angle
+     * brackets.
+     */
+    public Dictionary<string, string> Words { get; set; }
+
+    public DictionaryWords()
+    {
+      /**
+       * To create a new dictionary, we instantiate the class
+       * Dictionary passing the type of key and value between
+       * angle brackets.
+       * 
+       * We can then access the values using a syntax like this:
+       * 
+       * Words["key"], where "key" is the name of any of the keys
+       * added to the dictionary.
+       * 
+       * One constraint is that the key must be unique.
+       */
+      Words = new Dictionary<string, string>();
+    }
+  }
+}
---
+++ EssentialTraining/EssentialTrainingTests/DictionaryWordsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+  [TestClass]
+  public class DictionaryWordsTest
+  {
+    [TestMethod]
+    public void TestFunctionality()
+    {
+      var dict = new DictionaryWords();
+      dict.Words.Add("inaudible", "unable to be heard.");
+      dict.Words.Add("inch", "a unit of linear measure equal to one twelfth of a foot (2.54 cm).");
+      dict.Words.Add("metropolis", "the capital or chief city of a country or region.");
+
+      Assert.AreEqual("unable to be heard.", dict.Words["inaudible"]);
+      Assert.AreEqual(3, dict.Words.Count);
+    }
+  }
+}
