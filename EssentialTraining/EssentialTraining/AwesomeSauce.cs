8a6f92d545982d4bca96561c5241a2002c2e33ad
Victor Batalha <madivcb@gmail.com>
2020-03-08 23:04:53 -0300

    Lists

---
+++ EssentialTraining/EssentialTraining/AwesomeSauce.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EssentialTraining
+{
+  public class AwesomeSauce
+  {
+    /**
+     * Since List is a generic type, we have to pass the type
+     * between angle brackets just like in TypeScript.
+     */
+    public List<string> Sauces { get; set; }
+
+    public AwesomeSauce()
+    {
+      /**
+       * To create a list a use the List constructor and pass
+       * the type between angle brackets. We can then access
+       * the indexes like a normal array.
+       */
+      Sauces = new List<string>();
+    }
+
+    public bool IsSauceAwesome(string sauce)
+    {
+      /**
+       * Lists have more built in methods than a normal array.
+       */
+      return Sauces.Contains(sauce);
+    }
+  }
+}
---
+++ EssentialTraining/EssentialTrainingTests/AwesomeSauceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+  [TestClass]
+  public class AwesomeSauceTest
+  {
+    [TestMethod]
+    public void TestIsSauceAwesome()
+    {
+      var testInstance = new AwesomeSauce();
+      testInstance.Sauces.Add("Tobasco");
+      testInstance.Sauces.Add("Cholula");
+      testInstance.Sauces.Add("Trailer Trash");
+
+      Assert.IsTrue(testInstance.IsSauceAwesome("Cholula"), "it should return true if sauce is in the list");
+      Assert.IsFalse(testInstance.IsSauceAwesome("Barbecue"), "it should return false if sauce is not in the list");
+    }
+  }
+}
